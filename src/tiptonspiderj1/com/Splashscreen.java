34fa3ffc3c2710d0ec6cd1632d2af326763fc346
JEREMY TIPTON <32341894+tiptonspiderj@users.noreply.github.com>
2021-11-07 08:34:57 -0500

    Add files via upload

---
+++ src/tiptonspiderj1/com/Splashscreen.java
@@ -0,0 +1,19 @@
+package tiptonspiderj1.com;
+
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+
+public class Splashscreen {
+	
+	/***************************************************************************
+	*                                 Variables                                *
+	***************************************************************************/
+	
+	 @FXML
+	 private ImageView logoImage;
+	 
+	 @FXML
+	 private AnchorPane splashAnchorPane;
+
+}
